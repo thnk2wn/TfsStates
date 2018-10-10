680358027c73c9de94d34fe2096a25b4335019ec
Geoff Hudik <GeoffH@magenic.com>
2018-10-09 18:06:40 -0700

    Revert "Fix duplicate folder w/prior rename"
    This reverts commit 5f79ac3823ecffc86f4ff55a211e39e7b1430c3b.

---
+++ tfsstates/Controllers/SettingsController.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TfsStates.Models;
+using TfsStates.Services;
+
+namespace TfsStates.Controllers
+{
+    public class SettingsController : Controller
+    {
+        private const string ViewName = "~/Views/Settings/Index.cshtml";
+        private readonly ITfsSettingsService settingsService;
+
+        public SettingsController(ITfsSettingsService settingsService)
+        {
+            this.settingsService = settingsService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await this.settingsService.GetSettingsOrDefault();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SaveConnection(
+            [FromForm]
+            TfsConnectionModel model
+        )
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(ViewName, model);
+            }
+
+            model = await this.settingsService.Save(model, validate: true);
+
+            if (model.ValidationResult.IsError) { 
+                ModelState.AddModelError(string.Empty, model.ValidationResult.Message);
+            }
+
+            return View(ViewName, model);
+        }
+    }
+}
