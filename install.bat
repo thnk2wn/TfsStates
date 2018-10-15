46200335c51d28470d65acbdf14eef3431892e88
Geoff Hudik <GeoffH@magenic.com>
2018-10-15 14:05:57 -0700

    Add install.bat

---
+++ install.bat
@@ -0,0 +1,9 @@
+set installDir="%ALLUSERSPROFILE%\%USERNAME%\TfsStates"
+set appExe="%ALLUSERSPROFILE%\%USERNAME%\TfsStates\TfsStates.exe"
+
+pushd Files
+xcopy /s /i /y /r *.* %installDir%
+
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\TfsStates.lnk');$s.TargetPath='%appExe%';$s.Save()"
+
+popd
