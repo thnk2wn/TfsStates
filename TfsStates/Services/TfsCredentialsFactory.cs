6e6d51304ca32281b82554904ec36cd275e16d75
Geoff Hudik <GeoffH@magenic.com>
2018-10-17 09:16:17 -0700

    credentials TODO for devops support reminder later

--- TfsStates/Services/TfsCredentialsFactory.cs
+++ TfsStates/Services/TfsCredentialsFactory.cs
@@ -6,6 +6,9 @@ namespace TfsStates.Services
 {
     public static class TfsCredentialsFactory
     {
+        // TODO: Add support for OAuth and/or personal access token for Azure DevOps with changes to settings view
+        // https://docs.microsoft.com/en-us/azure/devops/integrate/get-started/client-libraries/samples?view=vsts
+        // https://stackoverflow.com/questions/46719764/cant-access-my-repos-in-vsts-using-rest-api
         public static VssCredentials Create(TfsConnectionModel model)
         {
             var creds = model.UseWindowsIdentity 
