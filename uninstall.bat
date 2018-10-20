e450480807d2b62616c5e738ffbc6a2b649f126a
Geoff Hudik <GeoffH@magenic.com>
2018-10-20 11:52:25 -0700

    Added uninstall script

---
+++ uninstall.bat
@@ -0,0 +1,18 @@
+@echo off
+
+set installDir="%ALLUSERSPROFILE%\%USERNAME%\TfsStates"
+set shortcut="%userprofile%\Start Menu\Programs\TfsStates.lnk"
+set dataDir="%appdata%\TfsStates"
+
+echo Install Dir: %installDir%
+echo Data Dir: %dataDir%
+echo Shortcut: %shortcut%
+
+echo Uninstall starting
+
+if exist %installDir% echo Removing %installDir% & rd /q /s %installDir%
+if exist %dataDir% echo Removing %dataDir% & rd /q /s %dataDir%
+if exist %shortcut% echo Removing %shortcut% & del /f %shortcut%
+
+echo Done, press Enter to exit
+prompt
